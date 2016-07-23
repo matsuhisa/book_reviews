bc45c23a1e151853d4665863c9915af4bfc1aa81
matsuhisa <matsuhisa+github@gmail.com>
2016-07-23 23:51:20 +0900

    rails generate scaffold Review title:string description:text user_id:integer book_id:integer

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,55 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  permits :title, :description, :user_id, :book_id
+
+  # GET /reviews
+  def index
+    @reviews = Review.all
+  end
+
+  # GET /reviews/1
+  def show
+  end
+
+  # GET /reviews/new
+  def new
+    @review = Review.new
+  end
+
+  # GET /reviews/1/edit
+  def edit
+  end
+
+  # POST /reviews
+  def create(review)
+    @review = Review.new(review)
+
+    if @review.save
+      redirect_to @review, notice: 'Review was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PUT /reviews/1
+  def update(review)
+    if @review.update(review)
+      redirect_to @review, notice: 'Review was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /reviews/1
+  def destroy
+    @review.destroy
+
+    redirect_to reviews_url, notice: 'Review was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_review(id)
+      @review = Review.find(id)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723144451) do
+ActiveRecord::Schema.define(version: 20160723145013) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.string   "author"
     t.string   "asin"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "reivews", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.integer  "book_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.integer  "book_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
