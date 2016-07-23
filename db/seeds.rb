6042106c3eb19299a1c2becb544355d4837d0665
matsuhisa <matsuhisa+github@gmail.com>
2016-07-24 01:53:36 +0900

    Add db/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Book
+[
+  { title: "イズムの功過", author: "夏目漱石" },
+  { title: "一夜", author: "夏目漱石" },
+  { title: "永日小品", author: "夏目漱石" },
+  { title: "薤露行", author: "夏目漱石" },
+  { title: "学者と名誉", author: "夏目漱石" },
+  { title: "硝子戸の中", author: "夏目漱石" },
+  { title: "虚子君へ", author: "夏目漱石" },
+  { title: "草枕", author: "夏目漱石" },
+  { title: "虞美人草", author: "夏目漱石" },
+  { title: "ケーベル先生", author: "夏目漱石" },
+  { title: "こころ", author: "夏目漱石" },
+  { title: "三四郎", author: "夏目漱石" },
+].each_with_index do |book, index|
+  Book.create(id:index+1, title: book[:title], author: book[:author])
+end
