c98913bc76a07c57956034af50affbf91d1ee323
matsuhisa <matsuhisa+github@gmail.com>
2016-08-11 23:31:35 +0900

    Update book db:migrate

--- db/migrate/20160723144451_create_books.rb
+++ db/migrate/20160723144451_create_books.rb
@@ -1,10 +1,10 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :description
       t.string :author
-      t.string :asin
+      t.string :asin, null: false
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@
 ActiveRecord::Schema.define(version: 20160725153848) do
 
   create_table "books", force: :cascade do |t|
-    t.string   "title"
+    t.string   "title",       null: false
     t.text     "description"
     t.string   "author"
-    t.string   "asin"
+    t.string   "asin",        null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
