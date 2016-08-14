2ab4b8018198a39dfacc486471d2d2d3875ce81f
matsuhisa <matsuhisa+github@gmail.com>
2016-08-15 00:11:08 +0900

    Setting config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   delete '/sign_out', to: 'sessions#destroy', as: 'sign_out'
   get '/auth/:provider/callback', to: 'sessions#create'
 
+  get '/my', to: 'my#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
