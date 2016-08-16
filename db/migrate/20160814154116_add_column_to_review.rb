8dff715da3aee1c622a138e97c64383ef3c121aa
matsuhisa <matsuhisa+github@gmail.com>
2016-08-17 00:59:38 +0900

    Change deleted_at from timestamps to datetime

--- db/migrate/20160814154116_add_column_to_review.rb
+++ db/migrate/20160814154116_add_column_to_review.rb
@@ -1,6 +1,6 @@
 class AddColumnToReview < ActiveRecord::Migration
   def change
-    add_column :reviews, :deleted_at, :timestamps
+    add_column :reviews, :deleted_at, :datetime
     add_column :reviews, :state, :integer, default: 1, null: false
   end
 end
